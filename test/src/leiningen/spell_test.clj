5dee47be6da3103f78859dbc8e15df74f321f45b
Gabriel Horner <gabriel.horner@gmail.com>
2013-06-24 13:51:39 -0400

    add unit tests for all the main whitelist cases

--- test/src/leiningen/spell_test.clj
+++ test/src/leiningen/spell_test.clj
@@ -2,9 +2,12 @@
   (:require [leiningen.spell :as spell]
             [clojure.java.shell :as sh]
             [clojure.java.io :as io]
+            [clojure.string]
             [bultitude.core :as b]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all])
+ (:import [java.io File]))
 
+;; put is inside of predicate otherwise clojure.test wouldn't report actual value
 (defn- has-correct-file-output?
   [output & files]
   (is (= output
@@ -44,5 +47,35 @@
                        file-seq (constantly '())]
            (spell/spell* '()))))))
 
-;; unit tests
-;; whitelist, core name, local whitelist, ns name, ignorable
+(defn- identifies-correct-typos?
+  [expected input & [nsp]]
+  (is (= expected
+         (spell/typos-for-file
+           (doto
+             (File/createTempFile (first expected) ".txt")
+             (#(spit % input)))
+           nsp))))
+
+
+(deftest typos-for-file-test
+  (testing "whitelist of whitelist.txt works"
+    (identifies-correct-typos? '("arested") "This is arested autoscaling development"))
+  (testing "whitelist of clojure core names"
+    (identifies-correct-typos? '("xpand") "To really xpand a macro, use macroexpand."))
+  (testing "whitelist of current ns names"
+    (def cowabunga)
+    (identifies-correct-typos? '("woah") "This ns could have names like woah and cowabunga." 'leiningen.spell-test))
+  (testing "whitelist of any words with caps"
+    (identifies-correct-typos? '("ridiculusly") "I Loled ridiculusly hard on your HTTPS joke."))
+  (testing "whitelist of apostrophe words"
+    (identifies-correct-typos? '("critterz")
+                               "Them critterz have mess'd with the wrong fellah's."))
+  (testing "whitelist of pluralized words"
+    (identifies-correct-typos? '("tigerrs")
+                               "Lions, tigerrs, defstructs and backends oh my."))
+  (testing "whitelist of .lein-spell"
+    (spit ".lein-spell" "defenestrate\n")
+    (require 'leiningen.spell :reload) ;; reload memoized whitelist
+    (identifies-correct-typos? '("anoher")
+                               "If he rhymes anoher clojar with clojure I swear I'm going to defenestrate him.")
+    (.delete (io/file ".lein-spell"))))
