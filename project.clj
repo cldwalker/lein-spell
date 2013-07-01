10a8c3bd62f727e6722a73e8fbd3cc84a2a9c702
Gabriel Horner <gabriel.horner@gmail.com>
2013-07-01 08:08:09 -0400

    add profiles + alias

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :eval-in :leiningen
   ;; because test/fixtures has broken nses that shouldn't be loaded by default
   :test-paths ["test/src"]
-  :dependencies [[bultitude "0.2.2"]])
+  :dependencies [[bultitude "0.2.2"]]
+  :profiles  {:1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.5"]})
